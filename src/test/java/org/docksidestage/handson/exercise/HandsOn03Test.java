07c0e0428810b784b18b1a53ba137d8b863bddbf
ayamin470 <ayami.hatano@bizreach.co.jp>
2026-02-25 11:25:04 +0900

    2/25：HandsOn03[6]まで解いた、TODOを捌いた

--- src/test/java/org/docksidestage/handson/exercise/HandsOn03Test.java
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn03Test.java
@@ -1,11 +1,15 @@
 package org.docksidestage.handson.exercise;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.annotation.Resource;
 
 import org.dbflute.cbean.result.ListResultBean;
+import org.dbflute.helper.HandyDate;
 import org.docksidestage.handson.dbflute.exbhv.MemberBhv;
 import org.docksidestage.handson.dbflute.exbhv.MemberSecurityBhv;
 import org.docksidestage.handson.dbflute.exbhv.MemberStatusBhv;
@@ -17,7 +21,6 @@ import org.docksidestage.handson.unit.UnitContainerTestCase;
  * @author ayamin
  * @author jflute
  */
-// TODO ayami.hatano コード補完：https://dbflute.seasar.org/ja/manual/function/ormapper/conditionbean/howto.html#completion  (2026/01/27)
 public class HandsOn03Test extends UnitContainerTestCase {
 
     @Resource
@@ -133,11 +136,9 @@ public class HandsOn03Test extends UnitContainerTestCase {
 
         // done ayami.hatano 2という「文字が含まれている」に注意 (2026/01/29)
         // done ayami.hatano 会員セキュリティ情報のデータ自体は要らない (2026/01/29)
-        // TODO ayami.hatano handson02 も要件を満たしているか確認する (2026/01/29)
-        // TODO ayami.hatano 曖昧性に気づくには？▶︎推測のまま動かない▶︎推測をしていることを自覚する、推測を確証する工程を踏む (2026/01/30)
+        // TODO done ayami.hatano 曖昧性に気づくには？▶︎推測のまま動かない▶︎推測をしていることを自覚する、推測を確証する工程を踏む (2026/01/30)
         // done ayamin 要件の対象カラムを間違えている (指差し確認しましょう) by jflute (2026/01/30)
         // done なのでUnitTestも落ちてる (実行してgreenを確認確認しましょう) by jflute (2026/01/30)
-        // TODO ayami.hatano linter使ってみる (2026/02/02)
         // #1on1: 関連テーブル側(1:1)のカラムでの絞り込みのやり方Good (2026/01/30)
         // cb.query()... は基点テーブルのカラムの絞り込みなので、
         // cb.query().queryMemberSecurityAsOne()... query,queryで繋げることで関連テーブルの絞り込みができる。
@@ -150,9 +151,10 @@ public class HandsOn03Test extends UnitContainerTestCase {
         assertHasAnyElement(memberList);
         for (Member member : memberList) {
             memberSecurityBhv.selectByPK(member.getMemberId()).alwaysPresent(security -> {
-                // TODO ayamin getReminderQuestion()を抽出してみましょう (IntelliJのcontrol+Tで抽出) by jflute (2026/02/13)
-                log("検索された会員: " + member.getMemberName() + " " + security.getReminderQuestion());
-                assertTrue(security.getReminderQuestion().contains("2"));
+                // TODO dobe ayamin getReminderQuestion()を抽出してみましょう (IntelliJのcontrol+Tで抽出) by jflute (2026/02/13)
+                String reminderQuestion = security.getReminderQuestion();
+                log("検索された会員: " + member.getMemberName() + " " + reminderQuestion);
+                assertTrue(reminderQuestion.contains("2"));
             });
         }
     }
@@ -174,7 +176,6 @@ public class HandsOn03Test extends UnitContainerTestCase {
     // done ayamin [読み物課題] 論理的矛盾が発生したら、思い込み前提を探す by jflute (2026/01/30)
     // https://jflute.hatenadiary.jp/entry/20180831/contradictionstep
 
-    // TODO jflute 次回1on1ここから (2026/01/30)
     // done ayami.hatano 聞く：「会員ステータスのデータ自体は要らない」場合、どのようにクエリを書くべき？ (2026/02/13)
     // cb.query().queryMemberStatus()では、CQ(検索条件のオブジェクト)が返ってきているので、これを使うと良さそう
     // #1on1: selectの目的が不要と言うことで、setupSelect_MemberStatus()を呼ばなければOK (2026/02/13)
@@ -197,13 +198,17 @@ public class HandsOn03Test extends UnitContainerTestCase {
 
         // ## Assert ##
         assertHasAnyElement(memberList);
-        // TODO ayamin Setで十分。重複のないリストを使ってみましょう by jflute (2026/02/13)
-        // 今まで登場してきた全一覧じゃなく、今まで登場してきた種類一覧で良い。
+
         // #1on1: 最初の時は、長めの変数名でわかりやすくでOK。慣れてくると自然に減らせるので (2026/02/13)
         // 最初から短めに寄せて増やしていくのは、何が必要なのか？何が不要なのか？が学びにくい。
-        // TODO ayamin StatusCodeってCodeであることをわかりやすくしてるなら、ここもStatusCodeListでは？ by jflute (2026/02/13)
-        List<String> historyStatusList = new ArrayList<>();
+
+        Set<String> historyStatusCodeSet = new HashSet<>();
         String previousStatusCode = null;
+        // TODO done ayamin Setで十分。重複のないリストを使ってみましょう by jflute (2026/02/13)
+        // setは重複するデータを保持できないルール。
+        // 「これまでに登場したステータスの種類を記憶しておき、後で再登場しないかチェックする」という目的に対して、setの方がパフォーマンス観点で良いし、コード見ただけでやりたいことわかる
+        // TODO done ayamin StatusCodeってCodeであることをわかりやすくしてるなら、ここもStatusCodeListでは？ by jflute (2026/02/13)
+
 
         for (Member member : memberList) {
             assertFalse(member.getMemberStatus().isPresent());
@@ -211,23 +216,73 @@ public class HandsOn03Test extends UnitContainerTestCase {
             String currentStatusCode = member.getMemberStatusCode();
             log("検索された会員: " + member.getMemberName() + ", 会員ID: " + member.getMemberId() + ", ステータス: " + member.getMemberStatusCode());
 
+            // TODO done ayamin ifがtrueだったらOUT, ってことは、そのifの条件がfalseでなきゃいけないってこと by jflute (2026/02/13)
+            // ってことは、contains()の戻り値が false であるはず、というアサートでも良いのでは？
+            // わかやすさの許容範囲内であれば、短い方が良い。
+            // 別に今の実装が悪いわけではなく、コードの変化を体験するトレーニングとして。
+            // ▶︎assertFalse使えばいいのか！
+
             if (previousStatusCode != null && !currentStatusCode.equals(previousStatusCode)) {
-                // TODO ayamin ifがtrueだったらOUT, ってことは、そのifの条件がfalseでなきゃいけないってこと by jflute (2026/02/13)
-                // ってことは、contains()の戻り値が false であるはず、というアサートでも良いのでは？
-                // わかやすさの許容範囲内であれば、短い方が良い。
-                // 別に今の実装が悪いわけではなく、コードの変化を体験するトレーニングとして。
-                if (historyStatusList.contains(currentStatusCode)) {
-                    fail("会員ステータスが固まって表示されていません: " + currentStatusCode + " が再登場しました");
-                }
+                assertFalse(historyStatusCodeSet.contains(currentStatusCode));
             }
 
-            historyStatusList.add(currentStatusCode);
+            historyStatusCodeSet.add(currentStatusCode);
             previousStatusCode = currentStatusCode;
         }
-        log(historyStatusList);
+        log(historyStatusCodeSet);
         
         // TODO jflute ↑次回1on1にて、違うやり方のふぉろー (2026/02/13)
-        
-        // #1on1: リーダブルコード読んでるって、すごい！ (2026/02/13)
+
+    }
+        // TODO ayami.hatano  setupsecectはテーブルのすべてのカラムを取得するので使えない (2026/02/17)
+        // TODO ayami.hatano 1行ずつ確認するやつ、まだやっていない (2026/02/17)
+    public void test_2005年10月の1日から3日までに正式会員になった会員を検索() {
+        // [6] 2005年10月の1日から3日までに正式会員になった会員を検索
+        // 画面からの検索条件で2005年10月1日と2005年10月3日がリクエストされたと想定して...
+        // Arrange で String の "2005/10/01", "2005/10/03" を一度宣言してから日時クラスに変換する
+        // 自分で日付移動などはせず、DBFluteの機能を使って、そのままの日付(日時)を使って条件を設定
+        // 会員ステータスも一緒に取得
+        // ただし、会員ステータス名称だけ取得できればいい (説明や表示順カラムは不要)
+        // 会員名称に "vi" を含む会員を検索
+        // 会員名称と正式会員日時と会員ステータス名称をログに出力
+        // 会員ステータスがコードと名称だけが取得されていることをアサート
+        // 会員の正式会員日時が指定された条件の範囲内であることをアサート
+
+        String fromDateString = "2005/10/01";
+        String toDateString = "2005/10/03";
+        LocalDateTime fromDate = new HandyDate(fromDateString).getLocalDateTime();
+        LocalDateTime toDate = new HandyDate(toDateString).getLocalDateTime();
+        String keyword = "vi";
+
+        ListResultBean<Member> memberList = memberBhv.selectList(cb -> {
+            cb.setupSelect_MemberStatus();
+            cb.specify().specifyMemberStatus().columnMemberStatusName();
+
+            //compareAsDate()よって、Toの条件を1日ずらし、< '2005-10-04 00:00:00'（10月4日の0時0分0秒より前、つまり10月3日の23時59分59秒まで）という条件のSQLを組み立ててくれている
+            //要は、DBに保存されているジフン病の情報を切り捨てる働き
+            cb.query().setFormalizedDatetime_FromTo(fromDate, toDate, op -> op.compareAsDate());
+            cb.query().setMemberName_LikeSearch(keyword, op -> op.likeContain());
+        });
+
+        assertHasAnyElement(memberList);
+        for (Member member : memberList) {
+            String memberName = member.getMemberName();
+            LocalDateTime formalizedDatetime = member.getFormalizedDatetime();
+            MemberStatus memberStatusCode = member.getMemberStatus().get();
+            //MemberStatusを取得するメソッドはOptionalEntity型を返す。ので、.get()で取り出す
+            String memberStatusName = memberStatusCode.getMemberStatusName();
+
+            log("検索された会員: " + memberName + ", 正式会員日時: " + formalizedDatetime + ", ステータス: " + memberStatusName);
+
+            assertNotNull(memberStatusCode.getMemberStatusCode());
+            assertNotNull(memberStatusName);
+            assertNull(memberStatusCode.getDescription());
+            assertNull(memberStatusCode.getDisplayOrder());
+
+            assertTrue(memberName.contains(keyword));
+            assertTrue(formalizedDatetime.isAfter(fromDate.minusSeconds(1)));
+            LocalDateTime nextDayOfToDate = toDate.plusDays(1);
+            assertTrue(formalizedDatetime.isBefore(nextDayOfToDate));
+        }
     }
 }
